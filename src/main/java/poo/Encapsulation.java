7cded2ac5fae3447a2e178822fda106cd6b62844
jhonatanmizu <natanjesuss20@gmail.com>
2025-01-06 23:31:59 -0300

    :sparkles: feat: create encapsulation class

--- src/main/java/poo/Encapsulation.java
+++ src/main/java/poo/Encapsulation.java
@@ -1,4 +1,23 @@
 package poo;
 
 public class Encapsulation {
+    private String name = "";
+    private String info = "";
+
+    public String getName() {
+        return name;
+    }
+
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getInfo() {
+        return info;
+    }
+
+    public void setInfo(String info) {
+        this.info = info;
+    }
 }
