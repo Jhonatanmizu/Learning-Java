29ad5515cf3014f403957a6b1bea9db43b0d0765
jhonatanmizu <natanjesuss20@gmail.com>
2025-01-14 20:03:30 -0300

    :sparkles: feat: use date and time exaples

---
+++ src/main/java/DateAndTime.java
@@ -0,0 +1,27 @@
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Date;
+
+public class DateAndTime {
+
+    public static void main(String[] args) {
+        Date dateNow = new Date();
+
+        LocalDate myLocalDate = LocalDate.now();
+        System.out.println("MY LOCAL DATE");
+        System.out.println(myLocalDate);
+
+        LocalDateTime myLocalDateTime = LocalDateTime.now();
+        System.out.println("MY LOCAL DATE AND TIME");
+        System.out.println(myLocalDateTime);
+
+        Instant myInstant = Instant.now();
+        System.out.println("MY INSTANT");
+        System.out.println(myInstant);
+
+        LocalDate textToDate = LocalDate.parse("2022-08-12");
+        System.out.println("TEXT TO DATE");
+        System.out.println(textToDate);
+    }
+}
